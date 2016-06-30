c247d54aea99382b7b1172f81a924538f514f4b5
Chungha <sch8906@gmail.com>
2016-06-30 18:34:45 -0400

    delete artemis

--- artemis-modified/WebKit/Tools/qmake/sync.profile
+++
@@ -1,4 +0,0 @@
-# This file is only used for config tests
-%configtests = (
-    fontconfig => {},
-);
--- artemis-modified/fetch-qt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget "https://download.qt.io/archive/qt/4.8/4.8.1/qt-everywhere-opensource-src-4.8.1.tar.gz"
-tar xvfz qt-everywhere-opensource-src-4.8.1.tar.gz && mv qt-everywhere-opensource-src-4.8.1 qt
-cd qt
-echo -e 'o\nyes\n' | ./configure -prefix 'pwd' -no-webkit
-make
